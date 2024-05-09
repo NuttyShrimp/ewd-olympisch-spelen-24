499ca39b9cb71ba2104d933d00456f38d4a92ed3
NuttyShrimp <jan.lecoutere@hotmail.be>
2024-05-09 16:58:44 +0200

    feat(tickets): add notification after buy

--- src/main/java/org/hogent/olympisch_spelen_24/controller/TicketController.java
+++ src/main/java/org/hogent/olympisch_spelen_24/controller/TicketController.java
@@ -1,8 +1,7 @@
 package org.hogent.olympisch_spelen_24.controller;
 
 import jakarta.validation.Valid;
-import org.hogent.olympisch_spelen_24.domain.Competition;
-import org.hogent.olympisch_spelen_24.domain.Ticket;
+import org.hogent.olympisch_spelen_24.domain.*;
 import org.hogent.olympisch_spelen_24.repository.CompetitionRepository;
 import org.hogent.olympisch_spelen_24.service.TicketService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Optional;
 
@@ -40,7 +40,7 @@ public class TicketController {
     }
 
     @PostMapping("/{comp_id}")
-    public String buyTicket(@PathVariable Long comp_id, @Valid Ticket ticket, BindingResult result, Model model) {
+    public String buyTicket(@PathVariable Long comp_id, @Valid Ticket ticket, BindingResult result, Model model, RedirectAttributes redirectAttributes) {
         Optional<Competition> competition = competitionRepository.findById(comp_id);
 
         if (competition.isEmpty()) {
@@ -48,7 +48,17 @@ public class TicketController {
             return "redirect:/404";
         }
 
+        if (result.hasErrors()) {
+            model.addAttribute("competition", competition.get());
+            return "ticket/buy";
+        }
         ticketService.createNew(ticket);
+
+        if (ticket.getCount() == 1) {
+            redirectAttributes.addFlashAttribute("notification", String.format("Bought %d ticket", ticket.getCount()));
+        } else {
+            redirectAttributes.addFlashAttribute("notification", String.format("Bought %d tickets", ticket.getCount()));
+        }
 
         return String.format("redirect:/sport/%d", competition.get().getSport().getId());
     }
